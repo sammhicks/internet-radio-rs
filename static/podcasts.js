6653f596daa5f763d06932499630f0ff9183fb06
Samuel Hicks <SamMHicks@hotmail.co.uk>
2020-07-13 22:54:38 +0100

    Improved Web App behaviour The first podcast is loaded when the page loads When you add a new podcast, it selects it Improved visual layout of podcast items

--- static/podcasts.js
+++ static/podcasts.js
@@ -11,14 +11,20 @@ const new_podcast = document.getElementById("new_podcast");
 const podcast_title = document.getElementById("podcast_title");
 const podcast_items = document.getElementById("podcast_items");
 
-podcasts_select.addEventListener("input", ev => {
+function fetch_podcast() {
     podcast_title.textContent = "";
 
     while (podcast_items.firstChild) {
         podcast_items.removeChild(podcast_items.lastChild);
     }
 
-    fetch(ev.target.selectedOptions[0].value).then(response => {
+    const selected_podcast = podcasts_select.selectedOptions[0];
+
+    if (selected_podcast === undefined) {
+        return;
+    }
+
+    fetch(selected_podcast.value).then(response => {
         if (!response.ok) {
             throw response.statusText;
         }
@@ -42,36 +48,38 @@ podcasts_select.addEventListener("input", ev => {
                     const subtitle_text = getChildText(channel_item, "itunes:subtitle");
                     const description_text = getChildText(channel_item, "description");
 
-                    const title_element = document.createElement("dt");
+                    const title_element = document.createElement("h2");
                     title_element.appendChild(document.createTextNode(title_text));
                     podcast_items.appendChild(title_element);
 
                     if (subtitle_text && subtitle_text != title_text) {
-                        const subtitle_element = document.createElement("dd");
+                        const subtitle_element = document.createElement("h3");
                         subtitle_element.appendChild(document.createTextNode(subtitle_text));
                         podcast_items.appendChild(subtitle_element);
                     }
 
                     if (description_text) {
-                        const description_element = document.createElement("dd");
+                        const description_element = document.createElement("p");
                         description_element.appendChild(document.createTextNode(description_text));
                         podcast_items.appendChild(description_element)
                     }
 
-                    const link_dd = document.createElement("dd");
                     const play_button = document.createElement("button");
                     play_button.appendChild(document.createTextNode("Play"));
                     const enclosure = channel_item.getElementsByTagName("enclosure")[0];
                     const link_href = enclosure ? enclosure.getAttribute("url") : channel_item.getElementsByTagName("link")[0].textContent;
                     play_button.addEventListener("click", () => { post("play_url", link_href); });
-                    link_dd.appendChild(play_button);
-                    podcast_items.appendChild(link_dd);
+                    podcast_items.appendChild(play_button);
+
+                    podcast_items.appendChild(document.createElement("hr"));
 
                     break;
             }
         }
-    })
-});
+    });
+}
+
+podcasts_select.addEventListener("input", fetch_podcast);
 
 function load_podcasts() {
     const current_podcasts_src = window.localStorage.getItem(LOCAL_STORAGE_PODCASTS_KEY);
@@ -105,13 +113,13 @@ function add_podcast() {
         });
 
         window.localStorage.setItem(LOCAL_STORAGE_PODCASTS_KEY, JSON.stringify(current_podcasts));
-        reload_podcasts();
+        reload_podcasts(true);
     });
 
     new_podcast.value = "";
 }
 
-function reload_podcasts() {
+function reload_podcasts(select_last) {
     while (podcasts_select.firstChild) {
         podcasts_select.removeChild(podcasts_select.lastChild);
     }
@@ -127,6 +135,12 @@ function reload_podcasts() {
 
         podcasts_select.appendChild(podcast_element);
     }
+
+    if (select_last) {
+        podcasts_select.selectedIndex = podcasts_select.options.length - 1;
+    }
+
+    fetch_podcast();
 }
 
 reload_podcasts();
